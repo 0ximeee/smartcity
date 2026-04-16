e098baa1c5f8e46c5abff48240f9f7c9f0fc1648
ximena campos <xa.camposmiranda@ugto.mx>
2026-04-16 10:23:38 -0600

    [ADDED]: se termina la fase uno, solo estructura visual

---
+++ assets/js/register.js
@@ -0,0 +1,25 @@
+document.getElementById('registerForm')?.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('confirmPassword').value
+
+    if(!name || !email || !password || !confirmPassword){
+        showAlert('registerAlert', 'Todos los datos son obligatorios')
+        return
+    }
+
+    if(password !== confirmPassword){
+        showAlert('registerAlert', 'Las contraseñas no son iguales')
+        return
+    }
+
+    //Simulación de registro
+    localStorage.setItem('userName', name)
+
+    showAlert('registerAlert', 'Registro Satisfactorio')
+    window.location.href = 'login.html'
+
+})
